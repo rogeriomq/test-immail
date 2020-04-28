271874c2c4004fd2e461498470f2fde5043ba868
Rogério Milhomens de Queiroz <rogerio.mq@gmail.com>
2020-04-28 10:59:23 -0300

    ⚙️Define default port

--- api/src/index.js
+++ api/src/index.js
@@ -3,9 +3,7 @@ import server from './server'
 
 dotenv.config()
 
-const port = process.env.SERVER_PORT
-
-console.log('port', port)
+const port = process.env.SERVER_PORT || 3333
 
 server.listen(port, () =>
   console.log(`🚀 Restfull API listening http://localhost:${port}`)
